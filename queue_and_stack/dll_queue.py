91f7809c0d5dd94e501b48b836af4a5982358e20
newbi462 <isthisemailfree@gmail.com>
2020-04-20 15:38:22 -0400

    Works but has a zero que bug

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,14 +7,42 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        # AWNSER: This would never be the right size, by natuer a que is more a liked list where you are always changing the site in relation to the next nodethis would
         # self.storage = ?
+        self.storage = DoublyLinkedList()
+        # ????? Why? storage =?
 
     def enqueue(self, value):
-        pass
+        #pass
+        return self.storage.add_to_head(value)
+        # DoublyLinkedList(self, value) != storage
 
     def dequeue(self):
-        pass
+        #pass
+        return self.storage.remove_from_tail()
+        # ??? why does this not need (self)
 
     def len(self):
-        print(self.size)
-        return self.size
+        #print(self.size)
+        #return self.size
+        return self.storage.length
+
+
+
+test = Queue()
+test.enqueue(5)
+test.enqueue(3)
+print(test.len())
+print(test.dequeue())
+print(test.len())
+print(test.dequeue())
+print(test.len())
+
+print(test.dequeue())
+print(test.len())
+
+"""
+I need to better understand what "storage" is
+*is "storage" == "fopbar", or a reserver, or standard value like index or prams?
+
+"""
